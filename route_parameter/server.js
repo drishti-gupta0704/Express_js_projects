809b1602b4a16255c9f96e5b64515a41fb957419
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2026-01-10 04:08:07 +0530

    adding logic for route parameters

---
+++ route_parameter/server.js
@@ -0,0 +1,27 @@
+
+
+const express = require("express");
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("Route Params Project");
+});
+
+
+app.get("/user/:id", (req, res) => {
+  const userId = req.params.id;
+  res.send(`User ID is ${userId}`);
+});
+
+
+app.get("/product/:category/:id", (req, res) => {
+  const { category, id } = req.params;
+  res.send(`Category: ${category}, Product ID: ${id}`);
+});
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
